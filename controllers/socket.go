d1d364805e7b2def5ae6c2dbb9d09772e6316c87
padma-pani-patra <padmapanipatra@gmail.com>
2021-08-24 23:24:49 +0530

    socket

--- controllers/socket.go
+++ controllers/socket.go
@@ -3,9 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
-	"github.com/its-dastan/go-blog/helper"
 	"github.com/its-dastan/go-blog/models"
-	"github.com/its-dastan/go-blog/service"
 	"log"
 	"net/http"
 	"time"
@@ -14,39 +12,24 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func Handler(w http.ResponseWriter, r *http.Request){
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
-	}
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-
 		log.Println(err)
-		helper.ResponseWithJson(w, http.StatusBadRequest, helper.Response{Code: http.StatusBadRequest, Msg: err.Error()})
-
 		return
 	}
-
 	for{
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
+
 		time.Sleep(time.Second*5)
 		var results []*models.Blog
-		err= service.GetBlogs(&results)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+
 		if err:= conn.WriteJSON(&results); err!= nil {
 			fmt.Println(err)
 			return
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/its-dastan/go-blog/controllers"
 	"github.com/its-dastan/go-blog/route"
 	"net/http"
 )
@@ -9,7 +10,7 @@ import (
 func main() {
 	r := route.NewRouter()
 	fmt.Println("Listening on 3000")
-	//http.HandleFunc("/ws", controllers.Handler)
+	http.HandleFunc("/ws", controllers.Handler)
 	_ = http.ListenAndServe(":3000", r)
 
 }
